5d03d2eb24862812f0ee774f95482503834e7101
Hans Hofner <hhofner@gmail.com>
2024-07-10 17:02:07 +0900

    init app

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header class="header">
+    <img alt="Vue logo" class="logo" src="@/assets/logo.png" height="125" />
+  </header>
+  <nav class="nav">
+    <ul>
+      <li>
+      <RouterLink to="/">Editor 1</RouterLink>
+      </li>
+      <li>
+      <RouterLink to="/editor2">Editor 2</RouterLink>
+      </li>
+    </ul>
+  </nav>
+
+  <RouterView />
+</template>
+
+<style scoped>
+  .header {
+    display: flex;
+    background: rgb(255, 255, 9);
+    width: 100%;
+  }
+  .nav {
+    padding: 1rem 2rem;
+    border-bottom: 1px solid #ccc;
+    ul {
+      display: flex;
+      gap: 1rem;
+      * {
+        text-decoration: underline;
+        font-size: 1.2rem;
+      }
+    }
+  }
+</style>
---
+++ src/views/Editor2View.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main class="main">
+    <h1>Second editor demo</h1>
+  </main>
+</template>
+
+<style scoped>
+.main {
+  padding: 2rem 3rem;
+}
+</style>
