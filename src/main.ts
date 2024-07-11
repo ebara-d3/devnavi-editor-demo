295b38607ddb89f3d35bf7d51c7c267eb18ef8bc
Meng Li <meng.li@monstar-lab.com>
2024-07-11 16:58:14 +0900

    Add markdown demo

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,28 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VMdEditor from '@kangc/v-md-editor';
+import '@kangc/v-md-editor/lib/style/base-editor.css';
+import vuepressTheme from '@kangc/v-md-editor/lib/theme/vuepress.js';
+import '@kangc/v-md-editor/lib/theme/style/vuepress.css';
+import VueMarkdownEditor from '@kangc/v-md-editor';
+import createKatexPlugin from '@kangc/v-md-editor/lib/plugins/katex/cdn';
+import enUS from '@kangc/v-md-editor/lib/lang/en-US';
+
+// Prism
+import Prism from 'prismjs';
+// highlight code
+import 'prismjs/components/prism-json';
+
+VMdEditor.use(vuepressTheme, {
+  Prism,
+});
+VueMarkdownEditor.use(createKatexPlugin());
+VueMarkdownEditor.lang.use('en-US', enUS);
 
 const app = createApp(App)
 
 app.use(router)
+app.use(VMdEditor)
 
 app.mount('#app')
--- src/views/Editor2View.vue
+++ src/views/Editor2View.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
+import {ref} from "vue";
+
+const text = ref('# Hello, Markdown editor! \n $$\\sum_{i=1}^n a_i=0$$')
 </script>
 
 <template>
   <main class="main">
-    <h1>Second editor demo</h1>
+    <h1>Markdown editor demo</h1>
+    <v-md-editor v-model="text" height="400px"></v-md-editor>
   </main>
 </template>
 
